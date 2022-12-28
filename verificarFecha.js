2f2bd82eb2dd1aba276176c027a3657050881de1
Ferney Barbosa <barbosaferney05@gmail.com>
2022-12-28 10:55:57 -0500

    Subiendo proyecto Nuevo Año

--- verificarFecha.js
+++ verificarFecha.js
@@ -2,14 +2,20 @@
 const sectFechas = document.querySelector('[dataSect-fechas]');
 const sectNewyear = document.querySelector('[dataSect-newyear]');
 const video = document.querySelector('.video-fondo');
+const musicYear = document.querySelector('.music-year');
 
 export function verificandoFechaActual(){
-    const fActual = new Date(); 
-    const fDestino = new Date('29, 12, 2022');
+    const actual = new Date(); 
 
-    if(fActual.getTime() == fDestino.getTime()){
+    const fechaActual = `${actual.getDate()} ${actual.getMonth() + 1} ${actual.getFullYear()}`;
+    const fechaEsperada = '29 12 2022';
+
+    if(fechaActual == fechaEsperada){
         sectFechas.style = 'display: none';
         sectNewyear.style = 'display: block';
         video.style = 'display: block';
+        musicYear.style = 'display: block';
+    } else {
+        console.log('No son iguales');
     }
 }
