de57c2a56acbab1eb83244cfae651ad42acb3316
Ferney Barbosa <barbosaferney05@gmail.com>
2022-12-29 18:54:37 -0500

    Guardando cambios

--- fecha-actual.js
+++ fecha-actual.js
@@ -1,6 +1,11 @@
 
 const fechaActualspan = document.querySelector('[dataFechaActualspan]');
 
+const sectFechas = document.querySelector('[dataSect-fechas]');
+const sectSecondsMins = document.querySelector('.sect-secondsMins');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+
 // FECHA ACTUAL
 
 export function mostrandoFechaActual(){
@@ -8,4 +13,34 @@ export function mostrandoFechaActual(){
     const resultFecha  = `${fechaSistem.getDate()} / ${fechaSistem.getMonth() + 1} / ${fechaSistem.getFullYear()}`;
 
     fechaActualspan.innerHTML = resultFecha;
+
+    faltanFive();
+}
+
+const faltanFive = () => {
+    const nuevaHoursActual = new Date();
+    
+    const fechaActualFin = `${nuevaHoursActual.getDate()} ${nuevaHoursActual.getMonth() + 1} ${nuevaHoursActual.getFullYear()} ${nuevaHoursActual.getHours()} ${nuevaHoursActual.getMinutes()} ${nuevaHoursActual.getSeconds()}`;
+    const fechaFinYear = '29 12 2022 18 50 0'; 
+    const ultimoMin = '29 12 2022 18 51 59'; 
+
+    if(fechaActualFin >= fechaFinYear && fechaActualFin <= ultimoMin){
+        
+        sectFechas.style = 'display: none';
+        sectSecondsMins.style = 'display: block';
+
+        window.addEventListener("load",function(){
+            var sonido = document.createElement("iframe");
+            sonido.setAttribute("src", "faltan.mp3");
+            sonido.style = 'width: 0px; height: 0px; position: absolute; margin-top: -40rem';
+            document.body.appendChild(sonido).onplay();
+        });
+
+        setInterval(() => {
+            const horaActual = new Date();
+    
+            minutes.innerHTML = `${('0' + horaActual.getMinutes()).slice(-2)}`;
+            seconds.innerText = `${('0' + horaActual.getSeconds()).slice(-2)}`;
+        }, 1000);
+    }
 }
--- verificarFecha.js
+++ verificarFecha.js
@@ -3,19 +3,21 @@ const sectFechas = document.querySelector('[dataSect-fechas]');
 const sectNewyear = document.querySelector('[dataSect-newyear]');
 const video = document.querySelector('.video-fondo');
 const musicYear = document.querySelector('.music-year');
+const sectSecondsMins = document.querySelector('.sect-secondsMins');
+
+const fechaActualspan = document.querySelector('[dataFechaActualspan]');
 
 export function verificandoFechaActual(){
     const actual = new Date(); 
 
-    const fechaActual = `${actual.getDate()} ${actual.getMonth() + 1} ${actual.getFullYear()}`;
-    const fechaEsperada = '29 12 2022';
+    const fechaActual = `${actual.getDate()} ${actual.getMonth() + 1} ${actual.getFullYear()} ${actual.getMinutes()}`;
+    const fechaEsperada = '29 12 2022 52';
 
     if(fechaActual == fechaEsperada){
+        sectSecondsMins.style = 'display: none';
         sectFechas.style = 'display: none';
         sectNewyear.style = 'display: block';
         video.style = 'display: block';
         musicYear.style = 'display: block';
-    } else {
-        console.log('No son iguales');
     }
 }
